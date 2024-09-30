1901b3a6024dc836762cbde33465cdc3b5ac92b5
EfimIvanenko <efimivanenko666@gmail.com>
2024-09-30 12:51:38 +0300

    Add files via upload

---
+++ Практическая работа по уроку Организация программ и методы строк.py
@@ -0,0 +1,6 @@
+﻿my_string = str(input("Введите Ваше имя: ")) #Введите Ваше имя
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(' ', ''))
+print(my_string [0])
+print(my_string [15])
