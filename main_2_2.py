3723ae3b2ca3296cc1b2c1964b3f0b13dc4827fe
EfimIvanenko <efimivanenko666@gmail.com>
2024-10-07 11:43:15 +0300

    Update main_2_2.py

--- main_2_2.py
+++ main_2_2.py
@@ -1,14 +1,9 @@
-# Определим входные данные
-first = int(input('Введите первое число: '))
-second = int(input('Введите второе число: '))
-third = int(input('Введите третье число: '))
-# Пункты задачи:
-# Если все числа равны между собой, то вывести 3
-# Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
-# Если равных чисел среди 3-х вообще нет, то вывести 0
+first = int(input())
+second = int(input())
+third = int(input())
 if first == second and first == third:
-    print('Все', 3, 'числа равны между собой')
+    print(3)
 elif first == second or first == third or second == third:
-    print(2, 'числа равны между собой')
+    print(2)
 else:
-    print('Обнаружено', 0, 'равных между собой чисел')
+    print(0)
