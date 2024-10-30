9a400e2a4380dd53b2f47343678c018e213c70e2
EfimIvanenko <efimivanenko666@gmail.com>
2024-10-30 19:29:27 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -3,13 +3,16 @@ def get_multiplied_digits(number):
 
     first = int(str_number[0])
 
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
+    if len(str_number) == 1:
+        return first
     else:
 
-        return first
+        if first == 0:
+            return get_multiplied_digits(int(str_number[1:]))
+        else:
+
+            return first * get_multiplied_digits(int(str_number[1:]))
 
 
 result = get_multiplied_digits(40203)
-
 print(result)
