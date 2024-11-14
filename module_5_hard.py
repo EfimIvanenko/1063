68afe69685c3cf8c4ebe1d445e64b494117c3be7
EfimIvanenko <efimivanenko666@gmail.com>
2024-11-14 18:42:49 +0300

    Add files via upload

---
+++ module_5_hard.py
@@ -0,0 +1,103 @@
+import hashlib
+from time import sleep
+
+
+class User:
+    def __init__(self, nickname, password, age):
+        self.nickname = nickname
+
+        self.password = int(hashlib.sha256(password.encode()).hexdigest(), 16)
+        self.age = age
+
+    def __str__(self):
+        return f"User(nickname={self.nickname}, age={self.age})"
+
+
+class Video:
+    def __init__(self, title, duration, adult_mode=False):
+        self.title = title
+        self.duration = duration
+        self.time_now = 0
+        self.adult_mode = adult_mode
+
+    def __str__(self):
+        return f"Video(title={self.title}, duration={self.duration}, adult_mode={self.adult_mode})"
+
+
+class UrTube:
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = None
+
+    def log_in(self, nickname, password):
+        password_hash = int(hashlib.sha256(password.encode()).hexdigest(), 16)
+        for user in self.users:
+            if user.nickname == nickname and user.password == password_hash:
+                self.current_user = user
+                print(f"{nickname} успешно вошёл в систему.")
+                return
+        print("Неверный логин или пароль")
+
+    def register(self, nickname, password, age):
+        if any(user.nickname == nickname for user in self.users):
+            print(f"Пользователь {nickname} уже существует")
+            return
+        new_user = User(nickname, password, age)
+        self.users.append(new_user)
+        self.current_user = new_user
+        print(f"Пользователь {nickname} зарегистрирован и вошёл в систему.")
+
+    def log_out(self):
+        self.current_user = None
+        print("Вы вышли из аккаунта.")
+
+    def add(self, *videos):
+        for video in videos:
+            if all(v.title != video.title for v in self.videos):
+                self.videos.append(video)
+
+    def get_videos(self, search_term):
+        return [video.title for video in self.videos if search_term.lower() in video.title.lower()]
+
+    def watch_video(self, title):
+        if not self.current_user:
+            print("Войдите в аккаунт, чтобы смотреть видео")
+            return
+
+        for video in self.videos:
+            if video.title == title:
+                if video.adult_mode and self.current_user.age < 18:
+                    print("Вам нет 18 лет, пожалуйста покиньте страницу")
+                    return
+
+                print("Начало видео:")
+                for second in range(video.time_now + 1, video.duration + 1):
+                    print(second, end=" ", flush=True)
+                    sleep(1)
+                print("Конец видео")
+                video.time_now = 0
+                return
+
+        print("Видео не найдено")
+
+
+ur = UrTube()
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+ur.add(v1, v2)
+
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
+
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
+
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
+
+ur.watch_video('Лучший язык программирования 2024 года!')
